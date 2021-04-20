d3d769d43b396e50748b2418690837c13604fef7
revathysivadas <revathysivadas@revathys-air.attlocal.net>
2021-04-19 23:01:28 -0700

    First commit --Added project

---
+++ seleniumTest/src/seleniumTest/devopsproject.java
@@ -0,0 +1,33 @@
+package seleniumTest;
+
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+public class devopsproject {
+
+	
+	public static void main(String[] args) {
+	System.setProperty("webdriver.chrome.driver","/Users/revathysivadas/Documents/Selenium-Rev/lib/chromedriver");
+	ChromeDriver driver = new ChromeDriver();
+	
+	driver.get("http://localhost:8080");
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	driver.findElement(By.linkText("About Us")).click();
+	System.out.println("title of page is: " + driver.getTitle());
+	
+	String test = driver.findElement(By.tagName("b")).getText();
+	System.out.println(test);
+	  
+	System.out.println("Test Succeeded!!");
+	
+	driver.quit();
+	
+	} 
+	}
+
